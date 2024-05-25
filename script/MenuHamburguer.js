b6559ecb504f413106185b7f7eef5d52aaac91d2
Ghabriel Souza <souzaghabriel195@gmail.com>
2024-05-25 18:21:27 -0300

    menu hamburguer adicionado

---
+++ script/MenuHamburguer.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const mobileMenu = document.getElementById("mobileMenu");
+    const navLinks = document.getElementById("navLinks");
+
+    function toggleMenu() {
+        mobileMenu.classList.toggle("active");
+        navLinks.classList.toggle("active");
+    }
+
+    mobileMenu.addEventListener("click", toggleMenu);
+});
